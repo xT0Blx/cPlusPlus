b6a96761078e06039e81e782ca99f6905d695cc8
Shizu <135083266+xT0Blx@users.noreply.github.com>
2024-10-30 17:30:32 +0800

    Update and rename READ_ME to BasicCoding.cpp

---
+++ BasicCoding.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    cout << "Hello World! \n";
+    cout << "Testing print statements in C++" << endl;
+    cout << "Testing different endline methods" <<endl;
+    //Comments in C++ are like this
+    /* 
+        Some aspects of other programming languages
+        seem to be similar such as comments and special slash char,
+        hopefully other stuff are similar such as function creation and
+        polymorphism
+    */
+
+    int memeNum = 420;
+    cout << memeNum << endl;
+    memeNum = 69;
+    cout << memeNum << " is now the new number" << endl;
+
+    return 0;
+}
